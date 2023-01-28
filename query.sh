74cf52a9468b923bb77796836e6c4d90b6dc1b97
SeokyungKim <kskyung0624@gmail.com>
2023-01-29 01:02:49 +0900

    refacotr : 로컬 버전 질의기에 대해서 리팩토링 수행

--- query.sh
+++ query.sh
@@ -1,31 +1,21 @@
 #!/bin/bash
 
-if [ $# -ne 2 ]
-then
+if [ $# -ne 2 ]; then
   echo "정답 질의 포맷이 잘못되었습니다!"
-  echo "백엔드 정답 검색 : RID [b or backend] 퀴즈번호"
-  echo "프론트엔드 정답 검색 : RID [f or frontend] 퀴즈번호"
+  echo "백엔드 정답 검색 : rid [b or backend] 퀴즈번호"
+  echo "프론트엔드 정답 검색 : rid [f or frontend] 퀴즈번호"
   exit 1
 fi
 
-TARGET=0
-if [ $1 == "b" -o $1 == "backend" ]
-then
-  TARGET="backend"
-fi
-
-if [ $1 == "f" -o $1 == "frontend" ]
-then
-  TARGET="frontend"
-fi
-
-if [ ${TARGET} == 0 ]
-then
-  echo "정답 질의 포맷이 잘못되었습니다!"
-  echo "백엔드 정답 검색 : RID [b or backend] 퀴즈번호"
-  echo "프론트엔드 정답 검색 : RID [f or frontend] 퀴즈번호"
-  exit 1
-fi
+TARGET=
+case $1 in
+  "b" | "backend" ) TARGET="backend";;
+  "f" | "frontend" ) TARGET="frontend";;
+  * ) echo "정답 질의 포맷이 잘못되었습니다!"
+      echo "백엔드 정답 검색 : rid [b or backend] 퀴즈번호"
+      echo "프론트엔드 정답 검색 : rid [f or frontend] 퀴즈번호"
+      exit 1
+esac
 
 NUMBER="$2"
-cat ~/RID/answer/$TARGET/$NUMBER.txt
+cat ~/rid/answer/$TARGET/$NUMBER.txt
