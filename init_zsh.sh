2a04f76f2b992a297341f54757640a8ad9fc1c59
SeokyungKim <kskyung0624@gmail.com>
2023-02-19 21:31:17 +0900

    bug : db.json 위치를 잡지 못하는 버그 해결

--- init_zsh.sh
+++ init_zsh.sh
@@ -7,7 +7,9 @@ chmod +x "${file_path}/${file_name}"
 
 echo "\n# 랜면디 정보 등록 시작" >> ~/.zshrc
 echo "echo '랜면디 명령어 정보 : rid -h'" >> ~/.zshrc
-echo "alias rid='${file_path}/${file_name}'" >> ~/.zshrc
+echo "export RID_PATH='${file_path}'" >> ~/.zshrc
+echo "alias rid='$RID_PATH/${file_name}'" >> ~/.zshrc
 echo "rid" >> ~/.zshrc
 echo "# 랜면디 정보 등록 끝\n" >> ~/.zshrc
 echo "랜면디 초기화 완료!"
+source ~/.zshrc
