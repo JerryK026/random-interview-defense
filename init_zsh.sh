069cf908f9687cc163f8d646657700ccc99ed513
SeokyungKim <kskyung0624@gmail.com>
2023-02-19 16:43:07 +0900

    docs : 랜면디 등록 양식 재수정

--- init_zsh.sh
+++ init_zsh.sh
@@ -6,7 +6,7 @@ file_name="rid"
 chmod +x "${file_path}/${file_name}"
 
 echo "\n# 랜면디 정보 등록 시작" >> ~/.zshrc
-echo "랜면디 명령어 정보 : rid -h" >> ~/.zshrc
+echo "echo '랜면디 명령어 정보 : rid -h'" >> ~/.zshrc
 echo "alias rid='${file_path}/${file_name}'" >> ~/.zshrc
 echo "rid" >> ~/.zshrc
 echo "# 랜면디 정보 등록 끝\n" >> ~/.zshrc
